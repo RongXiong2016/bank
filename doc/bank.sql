cf984eeb4727a3f919615c54871c950987326f71
RongXiong2016 <13588034686@163.com>
2018-03-17 09:45:25 +0800

    sql

--- doc/bank.sql
+++ doc/bank.sql
@@ -10,20 +10,36 @@ Target Server Type    : MYSQL
 Target Server Version : 50718
 File Encoding         : 65001
 
-Date: 2018-02-12 13:17:03
+Date: 2018-03-17 09:32:02
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for admin
+-- ----------------------------
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE `admin` (
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL COMMENT '姓名',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
+  `phone` varchar(11) DEFAULT NULL COMMENT '手机号',
+  `title` varchar(64) DEFAULT NULL COMMENT '岗位',
+  `description` varchar(255) DEFAULT NULL COMMENT '职位描述',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for product
 -- ----------------------------
 DROP TABLE IF EXISTS `product`;
 CREATE TABLE `product` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `product_code` varchar(255) DEFAULT NULL COMMENT '产品编号',
   `name` varchar(64) NOT NULL COMMENT '产品名称',
   `type` varchar(64) DEFAULT NULL COMMENT '产品类型',
   `status` varchar(10) DEFAULT NULL COMMENT '状态 可购买...',
+  `currency` varchar(255) DEFAULT NULL COMMENT '货币',
   `channel` varchar(10) DEFAULT NULL COMMENT '发行渠道',
   `risk_level` varchar(10) DEFAULT NULL COMMENT '风险等级',
   `return_rate` decimal(6,0) DEFAULT NULL COMMENT '收益率',
@@ -40,17 +56,21 @@ CREATE TABLE `product` (
 -- ----------------------------
 DROP TABLE IF EXISTS `project`;
 CREATE TABLE `project` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
   `name` varchar(64) DEFAULT NULL COMMENT '产品系列名',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `description` varchar(255) DEFAULT NULL COMMENT '项目描述',
+  `create_admin_id` bigint(10) DEFAULT NULL COMMENT '创建者id',
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `create_admin_id` (`create_admin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for trade
 -- ----------------------------
 DROP TABLE IF EXISTS `trade`;
 CREATE TABLE `trade` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
   `trade_no` varchar(64) DEFAULT NULL COMMENT '交易编号',
   `user_id` int(10) DEFAULT NULL COMMENT '用户id',
   `product_id` int(10) DEFAULT NULL COMMENT '理财产品id',
@@ -65,13 +85,14 @@ CREATE TABLE `trade` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL COMMENT '姓名',
-  `password` varchar(64) NOT NULL COMMENT '密码',
+  `id` bigint(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL COMMENT '姓名',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
   `id_card` varchar(18) DEFAULT NULL COMMENT '身份证',
   `account` varchar(255) DEFAULT NULL COMMENT '交易账户',
+  `email` varchar(64) DEFAULT NULL COMMENT '邮箱',
   `phone` varchar(20) DEFAULT NULL COMMENT '联系电话',
   `address` varchar(128) DEFAULT NULL COMMENT '住址',
   `create_time` datetime DEFAULT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
