1d79fcd3e886c09b939dfad9b94a0ac535bf25d1
zfan <zrfan@leqee.com>
2018-05-21 18:21:47 +0800

    'update'

---
+++ src/main/java/com/service/NoticeService.java
@@ -0,0 +1,7 @@
+package com.service;
+
+import com.domain.Notice;
+
+public interface NoticeService {
+    public void add(Notice notice);
+}
---
+++ src/main/java/com/service/impl/NoticeServiceImpl.java
@@ -0,0 +1,13 @@
+package com.service.impl;
+
+import com.domain.Notice;
+import com.service.NoticeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeServiceImpl implements NoticeService {
+    @Override
+    public void add(Notice notice) {
+
+    }
+}
